c84ba324b038cd2dcfd4644787b303323ebceeb4
juliana <juliana.hachmann@gmail.com>
2024-08-19 16:39:08 -0300

    fix: adicionado tema CSS do pacote Atalanta

--- src/main/java/ifsc/poo/lavacao/Main.java
+++ src/main/java/ifsc/poo/lavacao/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import atlantafx.base.theme.*;
 
 import java.io.IOException;
 
@@ -12,13 +13,17 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        
+        // Temas: https://mkpaz.github.io/atlantafx/#themes 
+        Application.setUserAgentStylesheet(new NordDark().getUserAgentStylesheet());
+        
         Parent root = FXMLLoader.load(getClass().getResource("view/FXMLVBoxMain.fxml"));
 
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
         stage.setTitle("Sistema de Vendas (JavaFX MVC)");
-        stage.setResizable(false);
+        stage.setResizable(true);
         stage.show();
     }
 
